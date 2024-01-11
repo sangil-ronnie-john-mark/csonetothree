a043604488a69906ca94e8d1d3cccbf9b7e305f5
Xyna <ronniejohnmarksangil@gmail.com>
2024-01-11 09:10:14 +0800

    "Added Miko in Title"

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Landicho',
+      title: 'Flutter Landicho Miko',
       theme: ThemeData(
         // This is the theme of your application.
         //
